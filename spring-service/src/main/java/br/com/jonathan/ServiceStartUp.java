d72dfc97d596948b84751219e4a5785d4928a224
brow-joe <jo_souza92@yahoo.com.br>
2017-02-16 20:42:12 -0200

    remove

--- spring-service/src/main/java/br/com/jonathan/ServiceStartUp.java
+++ spring-service/src/main/java/br/com/jonathan/ServiceStartUp.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-//https://github.com/VanRoy/spring-cloud-dashboard/issues/8
 @EnableAutoConfiguration
 @ComponentScan("br.com.jonathan")
 @EnableFeignClients
